3aa3312c1551443237e230943de47c2843c275ed
Wondwosen <revatoel@gmail.com>
2024-04-07 16:55:39 +0300

    The first 18 fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+# the first numbers for fibonacci series
+prev2 = 0 
+prev1 = 1
+
+print(prev2)
+print(prev1)
+
+for i in range(18):
+    newFib = prev2 + prev1
+    print(newFib)
+    prev2 = prev1
+    prev1 = newFib
+
+print('\n' 'Done')
+
+
