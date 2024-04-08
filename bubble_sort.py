5c503fb0f61a0e6be142ec1bb7fa1e6a871da95b
Wondwosen <revatoel@gmail.com>
2024-04-08 22:00:09 +0300

    check if already sorted

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,8 +3,14 @@ my_array = [64, 34, 25, 12, 22, 11, 90, 5]
 n = len(my_array)
 
 for i in range(n-1):
+    swapped = False
     for j in range(n-i-1):
         if my_array[j] > my_array[j+1]:
-            my_array[j], my_array[j+1] = my_array[j+1],my_array[j]   #python multiple assignment feature
+                my_array[j], my_array[j+1] = my_array[j+1],my_array[j]   #python multiple assignment feature
+                swapped = True
 
+    if not swapped:
+        break
 print("Sorted array: ", my_array)
+
+# bubble sort  could be optimized to break out of the loop if there's no swap in the inner loop
