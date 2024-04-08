8562b962e797c9663c5686a339c12dd327f89094
Wondwosen <revatoel@gmail.com>
2024-04-08 20:52:32 +0300

    swap and bubble sort

---
+++ swap.py
@@ -0,0 +1,14 @@
+x = 47
+y = 00
+
+print('Before swapping \n')
+print('x:', x, '\n')
+print('y:', y, '\n')
+
+temp = x
+x = y
+y = temp
+
+print('After swapping')
+print('x:', x, '\n')
+print('y:', y, '\n')
